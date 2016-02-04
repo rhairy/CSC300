d55a2986ba14a5e61b010745f200fd48a6f4f741
rhairy <randall.harrison91@gmail.com>
2016-02-03 21:38:40 -0800

    Added working implementation for Two.

---
+++ AssignmentOne/Two/main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+void inSort(int *a, int s)
+{
+	//int tmp;
+
+	for (int i = 1; i < s; i++) {
+		
+		for (int j = i; j > 0; j--) {
+			if (a[j] < a[j-1]) {
+				/*
+				tmp = a[j-1];
+				a[j-1] = a[j];
+				a[j] = tmp;
+				*/
+				swap(a[j], a[j-1]);
+			} else {
+				continue;
+			}
+		}
+
+	}
+}
+
+void arrayPrint(int *a, int s)
+{
+	for (int i = 0; i < s; i++) {
+		cout << a[i]; 
+	}
+	cout << "" << endl;
+}
+
+template<typename T>
+void inTSort(T *t, int s)
+{
+	for (int i = 1; i < s; i++) {
+
+		for (int j = i; j > 0; j--) {
+			if (t[j] < t[j-1]) {
+				swap(t[j], t[j-1]);
+			} else {
+				continue;
+			}
+		}
+	}
+}
+
+template<typename T>
+void arrayTPrint(T *t, int s)
+{
+	for (int i = 0; i < s; i++) {
+		cout << t[i];
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	int myArray[10] = {5,0,3,6,2,7,1,8,9,4};
+
+	arrayTPrint(myArray, 10);
+	inTSort(myArray, 10);
+	arrayTPrint(myArray, 10);
+
+	
+	return 0;
+}
